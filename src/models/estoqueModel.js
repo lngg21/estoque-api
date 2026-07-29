1e47b41f9625d52528714398b0dd1995cac52817
lngg21 <arthur.vinigamer@gmail.com>
2026-07-29 15:59:16 -0300

    feat: cria model de estoque lendo regras do submodulo

---
+++ src/models/estoqueModel.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const regrasPath = path.join(__dirname, '../../contracts/regras-produtos.json');
+const regras = JSON.parse(fs.readFileSync(regrasPath, 'utf8'));
+let inventario = [
+    { sku: "PROD-1", qtd: 15 }
+];
+module.exports = {
+    obterEstoque: () => inventario,
+    verificarAlerta: (qtdAtual) => qtdAtual <= regras.alerta_estoque_baixo
+};
