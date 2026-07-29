1c3457ed08494915694d5834d4fe0b6642897a23
lngg21 <arthur.vinigamer@gmail.com>
2026-07-29 15:49:23 -0300

    feat: integras rotas de usuario da api

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const userRoutes = require('./src/routes/userRoutes');
 const app = express();
 app.use(express.json());
+app.use('/api/usuarios', userRoutes); 
 app.listen(3000, () => console.log('API rodando na porta 3000'));
