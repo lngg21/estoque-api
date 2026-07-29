4aee8b99cc058907d06b2b57db92676f745a3280
lngg21 <arthur.vinigamer@gmail.com>
2026-07-29 16:07:39 -0300

    feat: integra endpoint de status do estoque

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const userRoutes = require('./src/routes/userRoutes');
 const app = express();
+const estoqueRoutes = require('./src/routes/estoqueRoutes');
 app.use(express.json());
 app.use('/api/usuarios', userRoutes); 
+app.use('/api/estoque', estoqueRoutes);
 app.listen(3000, () => console.log('API rodando na porta 3000'));
